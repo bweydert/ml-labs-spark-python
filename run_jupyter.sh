3495486a060bca0fa128e236392ecfbd5a246d37
Sujee Maniyam (sujee-mac2) <sujee@sujee.net>
2017-08-27 01:44:58 -0700

    basics testing and jupyter [1:30]

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-## one liner
- #PYSPARK_PYTHON=python3 PYSPARK_DRIVER_PYTHON="jupyter" PYSPARK_DRIVER_PYTHON_OPTS="notebook" ~/spark/bin/pyspark
+
+# optional, if you want to use Anaconda python set the PATH here
+#export PATH=$HOME/anaconda/bin:$PATH
 
 #export SPARK_HOME=/Users/sujee/apps/spark
-export PYSPARK_PYTHON=python3 
-export PYSPARK_DRIVER_PYTHON="jupyter" 
-export PYSPARK_DRIVER_PYTHON_OPTS="notebook" 
+export PYSPARK_PYTHON=python3
+export PYSPARK_DRIVER_PYTHON="jupyter"
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
+
+# launch!
 ~/spark/bin/pyspark
+
+
+## one liner
+ #PYSPARK_PYTHON=python3 PYSPARK_DRIVER_PYTHON="jupyter" PYSPARK_DRIVER_PYTHON_OPTS="notebook" ~/spark/bin/pyspark
