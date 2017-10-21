aaf534179981c23fbd373c028499ebeaee560ab0
Sujee Maniyam <SUJEE@SUJEE.NET>
2017-10-21 01:21:30 -0700

    lab bundle script

--- bundle-labs.sh
+++ bundle-labs.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+## install nbconvert as
+##      conda install nbconvert
+##              or
+##      pip install nbconvert
+
+## cleanup html
+find . -name "*.html" -print0 | xargs -0 rm -f
+
 ## convert ipynb notebooks into HTML
-notebooks=$(find . -type f -name "*.ipynb" )
+notebooks=$(find . -type f -name "*.ipynb" | grep -v ".ipynb_checkpoints" )
 
-for nb in $notebooks
-do
-    echo "$nb"
-    jupyter nbconvert "$nb"
-done
+jupyter nbconvert ${notebooks}
 
+## Change all the links from README.html
+sed 's/ipynb/html/g' < README.html > a.html
+mv -f a.html  README.html
 
 # create a zipfile
-git archive --format=zip HEAD -o ml-labs-spark-python.zip
+
+rm -f ml-labs-spark-python.zip
+zip -r ml-labs-spark-python.zip . -x *.git*   -x "*metastore_db*"  -x "*derby.log"  -x "*.out"  -x "*.zip"  -x "*.ipynb_checkpoints*"
