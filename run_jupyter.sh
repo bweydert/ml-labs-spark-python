a0cd6b0cc5fd2d6907fabc0036b4115b108b281b
Sujee Maniyam @ MacAir2 <sujee@sujee.net>
2017-12-05 22:10:22 -0600

    run jupyter script tweak [1.0]

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -4,13 +4,9 @@
 #export PATH=$HOME/anaconda/bin:$PATH
 
 #export SPARK_HOME=/Users/sujee/apps/spark
-export PYSPARK_PYTHON=python3
-export PYSPARK_DRIVER_PYTHON="jupyter"
+export PYSPARK_PYTHON=$(which python3)
+export PYSPARK_DRIVER_PYTHON=$(which jupyter)
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
 
 # launch!
 ~/spark/bin/pyspark
-
-
-## one liner
- #PYSPARK_PYTHON=python3 PYSPARK_DRIVER_PYTHON="jupyter" PYSPARK_DRIVER_PYTHON_OPTS="notebook" ~/spark/bin/pyspark
