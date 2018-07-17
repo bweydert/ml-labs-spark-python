2df7cba050e28a6b5c22c322aa68cc72cd8b6644
Sujee Maniyam (macpro2) <sujee@sujee.net>
2018-07-16 23:06:36 -0700

    update run-jupyter [0.5]

--- run-jupyter.sh
+++ run-jupyter.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# optional, if you want to use Anaconda python set the PATH here
+## optional, if you want to use  python set the PATH here
 #export PATH=$HOME/anaconda/bin:$PATH
 
-#export SPARK_HOME=/Users/sujee/apps/spark
+export SPARK_HOME=$HOME/spark
+## better to specify full path
+#export PYSPARK_PYTHON="python3"
+#export PYSPARK_DRIVER_PYTHON="jupyter"
 export PYSPARK_PYTHON=$(which python3)
 export PYSPARK_DRIVER_PYTHON=$(which jupyter)
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
 
-# launch!
-~/spark/bin/pyspark
+## pyspark i local mode
+$SPARK_HOME/bin/pyspark
+
+## pyspark connect to cluster
+#$SPARK_HOME/bin/pyspark --master spark://localhost:7077  --executor-memory 4g --driver-memory 1g
