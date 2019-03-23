241177bdb249a648fb9eda644770e68399d5202a
Sujee Maniyam <sujee@sujee.net>
2019-03-23 07:01:01 -0500

    pacakge labs script updated

--- package-labs.sh
+++ package-labs.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
 
-## install nbconvert as
-##      conda install nbconvert
-##              or
-##      pip install nbconvert
+$ES_HOME/utils/package-labs.sh "$@"
 
-## cleanup html
-find . -name "*.html" -print0 | xargs -0 rm -f
-
-## convert ipynb notebooks into HTML
-notebooks=$(find . -type f -name "*.ipynb" | grep -v ".ipynb_checkpoints" )
-
-jupyter nbconvert ${notebooks}
-
-## Change all the links from README.html
-sed 's/ipynb/html/g' < README.html > a.html
-mv -f a.html  README.html
-
-# create a zipfile
-
-zip_file_name=$(basename `pwd`)
-rm -f *.zip
-zip -x '*.DS_Store*'  -x "*.log" -x '*.git*'  -x '*zip*'  -x '*metastore_db*' -x '*out' -x '*.ipynb_checkpoints*' -x '*not-using*' -r "$zip_file_name" .
-
-## old zip methods
-#rm -f ../$zip_file_name.zip
-#(cd .. ; zip -r ${zip_file_name}.zip ${zip_file_name} -x *.git*   -x *metastore_db*  -x *derby.log*  -x *.out  -x *.zip  -x *.ipynb_checkpoints*  ;  mv ${zip_file_name}.zip ${zip_file_name}  )
-#git archive --format=zip HEAD -o ml-labs-spark-python.zip
